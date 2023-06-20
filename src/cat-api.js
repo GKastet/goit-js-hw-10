ab7540ee8475dd0c5135c6596f273519e219b7c7
GKastet <127600582+GKastet@users.noreply.github.com>
2023-06-20 08:29:10 +0200

    hw-js-10

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,19 +1,11 @@
-//x-api-key
-//'api_key=live_SJWzhgZgj5uFzJ1aduBYqjxoRlvofKSr4uT2gAA9cSTe9uOKC5ffekGNalTXFO54'
-//https://api.thecatapi.com/v1/images/search?api_key=YOUR_API_KEY
 const BASE_URL = `https://api.thecatapi.com/v1`;
 const API_KEY = 'api_key=live_SJWzhgZgj5uFzJ1aduBYqjxoRlvofKSr4uT2gAA9cSTe9uOKC5ffekGNalTXFO54';
 
 function fetchBreeds(){
     const END_POINT =`/breeds`;
     const URL = `${BASE_URL}${END_POINT}?${API_KEY}`;
-    //console.log(URL);
-
-    return fetch(URL).then((responce)=>{
-        //console.log(responce);
-        if(!responce.ok){
-            reject();
-        }
+    
+    return fetch(URL).then((responce)=>{        
         return responce.json();
     })
 }
@@ -22,13 +14,11 @@ function fetchCatByBreed(breedId){
     const END_POINT = `/images/search`;
     const SELECT_BREED = '?breed_ids';
     const URL_BREED = `${BASE_URL}${END_POINT}${SELECT_BREED}=${breedId}&${API_KEY}`
-    //console.log(URL_BREED);
-
-    return fetch(URL_BREED).then((responce)=>{
-        //console.log(responce);
+    
+    return fetch(URL_BREED).then((responce)=>{        
         return responce.json();
     })
 }
-fetchCatByBreed()
+
 export {fetchBreeds};
 export {fetchCatByBreed};
--- src/index.js
+++ src/index.js
@@ -9,18 +9,21 @@ const elements = {
 
 elements.loader.hidden = false;
 elements.error.hidden = true;
-//elements.select.setAttribute('hidden', '')
 elements.catInfo.setAttribute('hidden', '')
 
 fetchBreeds().then((results)=>{
     elements.loader.hidden = true;
     elements.select.removeAttribute('hidden');
     elements.select.insertAdjacentHTML('beforeend', createMarkUpSelect(results))    
+}).catch(error =>{
+    elements.select.setAttribute('hidden', '');
+    elements.error.hidden = false;
+
+    console.log('error');
 });
 
 function createMarkUpSelect(arr){
     return arr.map(element => {
-        //console.log(element);
         return `<option value="${element.id}">${element.name}</option>`               
     });    
 }
@@ -29,22 +32,21 @@ elements.select.addEventListener('change', handlerSelect);
 
 function handlerSelect(evt){
     elements.catInfo.setAttribute('hidden', '')
-    //console.log(evt.target.value);
     const catBreed = evt.target.value
     elements.loader.hidden = false;
-    fetchCatByBreed(catBreed).then((results)=>{
-        //console.log(results);
+    fetchCatByBreed(catBreed).then((results)=>{        
         elements.loader.hidden = true;
         createMarkUp(results)
         elements.catInfo.removeAttribute('hidden');
         elements.catInfo.innerHTML = createMarkUp(results);
+    }).catch(error =>{
+        elements.select.setAttribute('hidden', '');
+        elements.error.hidden = false;
     }) 
 }
 
-function createMarkUp(arr){
-    //console.log(arr[0]);
-    return arr.map(({url, breeds}) => {
-        //console.log(breeds[0].temperament);
+function createMarkUp(arr){   
+    return arr.map(({url, breeds}) => {        
         const {name, description, temperament} = breeds[0];
         return `
         <img src="${url}" alt="#" width = "300">
